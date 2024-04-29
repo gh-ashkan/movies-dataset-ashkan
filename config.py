f4e8c8a520b38a7f8532a2e3a28dbad8397f7285
gh-ashkan <153124065+gh-ashkan@users.noreply.github.com>
2024-04-29 18:54:44 +0330

    Update config.py
    add print

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 OBJECT_CLASS = {
     "film": "film",
     "serial": "%d8%af%d8%a7%d9%86%d9%84%d9%88%d8%af-%d8%b3%d8%b1%db%8c%d8%a7%d9%84",
+    print("hello")
 }
 
 STORAGE = 'mongo'  # choices: ['mongo', 'file']
